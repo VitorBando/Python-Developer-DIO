9aae965e2ae4b6fae0d5836199120048fca7afe1
Vitor Bando <vitorm.804@gmail.com>
2023-12-07 23:59:24 -0300

    aprendendo um pouco sobre os operadores em Python.

---
+++ Fundamentos/operadores_associacao.py
@@ -0,0 +1,6 @@
+frutas = ['limao', 'uva', 'laranja']
+curso = 'Curso de Python'
+
+print('laranja' not in frutas)
+print('limao' in frutas)
+print('Python' in curso)
---
+++ Fundamentos/operadores_atribuicao.py
@@ -0,0 +1,17 @@
+saldo = 500
+print(saldo)
+
+saldo += 200
+print(saldo)
+
+saldo -=400
+print(saldo)
+
+saldo *= 3
+print(saldo)
+
+saldo /= 2
+print(saldo)
+
+saldo **= 3
+print(saldo)
