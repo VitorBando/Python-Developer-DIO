b092f9238fe2d6a927faa46d2ebcc23bff5c75f5
Vitor Bando <vitorm.804@gmail.com>
2024-03-01 16:45:49 -0300

    por dentro dos construtores e destrutores em classes/metodos.

---
+++ Classes e Objetos/contrutores_destrutores.py
@@ -0,0 +1,15 @@
+class Cachorro:
+    def __init__(self, nome, cor, acordado=True):
+        print('Inicializando a classe...')
+        self.nome=nome
+        self.cor=cor
+        self.acordado = acordado
+
+    def __del__(self):
+        print('Removendo a instância da classe.')        
+
+    def latir(self):
+        print('Auau')
+    
+c = Cachorro('chappie', 'caramelo')            
+c.latir()
